417e421959f6363e622682c5d2ed9dd7fb9ed274
Nando13 <nandoclb13@gmail.com>
2026-02-21 18:53:21 -0600

    Roque Robledo Fernando Ignacio ej1.c

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -8,3 +8,17 @@ int main(){
     }
     return 0;
 }
+//Direcciones impresas:
+//La direccion de a[0] es: 0061FF04
+//La direccion de a[1] es: 0061FF08
+//La direccion de a[2] es: 0061FF0C
+//La direccion de a[3] es: 0061FF10
+//La direccion de a[4] es: 0061FF14
+//La direccion de a[5] es: 0061FF18
+
+//Diferencia entre direcciones consecutivas:
+//La diferencia es de 0x4 en hexadecimal, es decir, 2 bytes de diferencia.
+
+//Fórmula general para dirección de a[i].
+//Dirección(a[i]) = &a[0] + i * sizeof(int)
+//En este caso al ser el tamaño de 4 bytes la fórmula es: Dirección de a[i]=&a[0]+i*4
