22cef19b32966aa8c16a0b0647ce926a1be4312b
Nando13 <nandoclb13@gmail.com>
2026-02-24 21:04:21 -0600

    Roque Robledo Fernando Ignacio ej5.c

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -1,6 +1,8 @@
+//Si a[0] = 1000 entonces a[10] = 1010.
+//Demuestra formalmente si es correcto.
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 5
-    return 0;
-}
+//Dirección a[10] = {Base} + (10 x Tamaño del tipo)
+//1010 = 1000 + (10 x Tamaño)
+//10 = 10 x Tamaño
+//{Tamaño} = 1 byte
+
