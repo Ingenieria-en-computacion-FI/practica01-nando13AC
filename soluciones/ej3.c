f9884845ff654009cbd0322fd6310b6659125759
Nando13 <nandoclb13@gmail.com>
2026-02-24 23:05:49 -0600

    Roque Robledo Fernando Ignacio ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -25,3 +25,26 @@ int main(){
 
     return 0;
 }
+
+
+//1. Imprime todas las direcciones.
+//Direcciones del arreglo a:0061FF08
+//Direcciones del arreglo a:0061FF0C
+//Direcciones del arreglo a:0061FF10
+
+//Direcciones del arreglo b:0061FEFC
+//Direcciones del arreglo b:0061FF00
+//Direcciones del arreglo b:0061FF04
+
+//Direcciones del arreglo c:0061FEF0
+//Direcciones del arreglo c:0061FEF4
+//Direcciones del arreglo c:0061FEF8
+
+//2. ¿Están juntos en la memoria?
+//No es seguro que las direcciones estén juntas en la memoria, pues podrían aparecer de forma inversa o con saltos,
+//por lo que es más seguro que estén cerca, pero no juntos. 
+
+
+//3. ¿De qué depende?
+//Depende de la alineación de memoria, pues el procesador lee en bloques de 4 o de 8 bytes.
+//Si el arreglo termina en un valor distinto se añaden espacios vacíos antes de empezar con el siguiente.
