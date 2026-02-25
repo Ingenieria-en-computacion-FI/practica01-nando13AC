5161151084196d5aa3782ca09631da90ed048feb
Nando13 <nandoclb13@gmail.com>
2026-02-24 22:33:45 -0600

    Roque Robledo Fernando Ignacio ej8.c

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,20 @@
+//Datos:
+//base = 2000
+//sizeof(int)=4
+//m[1][2] = 2024
+//Calcula:
+//Número de columnas
+//Usando la formula: Dir(m[i][j])=Base + ((i⋅Columnas+j)⋅sizeof(int)), sustituimos con la información que tenemos,
+//2024=2000+((Columnas+2)*4, 24/4 =Columnas+2, 6-2=Columnas, Columnas = 4. El número de columnas es 4
+//Tamaño total
+//Como el índice dado es m[1][2], sabemos que:
+//i = 1 → hay al menos 2 filas (0 y 1)
+//Entonces la matriz es de:0
+//2 filas × 4 columnas
+//Total de elementos:
+//2⋅4=8
+//Cada elemento ocupa 4 bytes:
+//8⋅4=32
+//Siendo el tamaño de la matriz 32 bytes
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 8
-    return 0;
-}
+//EN ESTE EJERCICIO NO SE HIZO CODIGO YA QUE TODO FUE A BASE DE CALCULOS
