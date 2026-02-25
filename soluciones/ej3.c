dff44120a45ca90f04a35da7fcd6492a2a816f91
Nando13 <nandoclb13@gmail.com>
2026-02-24 19:59:33 -0600

    Roque Robledo Fernando Ignacio ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,27 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 3
+    int a[3];
+    
+    for(int i = 0; i<3; i++){
+        printf("Direcciones del arreglo a:%p\n", &a[i]);
+    }
+
+    printf("\n");
+
+    int b[3];
+
+    for(int i = 0; i<3; i++){
+        printf("Direcciones del arreglo b:%p\n", &b[i]);
+    }
+
+    printf("\n");
+
+    int c[3];
+
+    for(int i = 0; i<3; i++){
+        printf("Direcciones del arreglo c:%p\n", &c[i]);
+    }
+
     return 0;
 }
