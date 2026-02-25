b51c13bd958c92656b708f737b0f4b2b354963be
Nando13 <nandoclb13@gmail.com>
2026-02-24 23:49:57 -0600

    Roque Robledo Fernando Ignacio ej9.c

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -8,3 +8,19 @@ int main(){
     
     return 0;
 }
+
+//Por qué compila
+//En C, el compilador no verifica los límites del arreglo en tiempo de compilación.
+
+//Por qué es peligroso
+//Porque estás accediendo a memoria fuera del bloque reservado para el arreglo. Eso puede:
+//Corromper otra variable
+//Alterar valores del stack
+//Causar segmentation fault
+//Generar comportamiento indefinido
+
+//Qué revela sobre memoria
+//Revela que:
+//C permite aritmética de apuntadores sin protección.
+//El lenguaje no implementa control automático de límites.
+//El programador es responsable del manejo correcto de memoria.
