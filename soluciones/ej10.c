74ceae49cc41a7b3164fefccd93f356152d723f4
Nando13 <nandoclb13@gmail.com>
2026-02-24 23:03:18 -0600

    Roque Robledo Fernando Ignacio ej10.c

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,19 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 10
+    int gigante[10000000];
+    printf("Direccion de memoria de: %p", &gigante);
+
     return 0;
 }
+
+//Si falla:
+//1. explica causa
+//La principal causa es el desbordamiento de la pila, al declarar “int gigante[1000000]” dentro de main,
+//pedimos que se reserven 10 millones de enteros por 4 bytes cada uno de golpe. 
+
+//2. indica dónde se almacena
+//Se almacena en el Stack o en la Pila 
+
+//3. propone solución sin memoria dinámica
+//Declarar un arreglo, como una variable global o usando “static”
